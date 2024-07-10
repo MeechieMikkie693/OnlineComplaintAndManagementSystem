ca700d6cbf6fc2770a665c8f5a021ec290a12aa8
MikkieGIT <114986541+MeechieMikkie693@users.noreply.github.com>
2024-07-10 20:50:33 +0530

    Changed the app.js file as required

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,45 @@
-const bcrypt = require('bcrypt')
-const express = require('express')
-const mongoose = require('mongoose')    //including mongoose
-const userRouter = require('./userRoutes')
-const cors = require('cors')
-const app = express()
-const PORT = 8000;
+//establishing our server
+const cors = require("cors");
+const express = require("express");
+const mongoose = require('mongoose');
+//const userRouter = require('./routes/userRouter');
+const studentRoutes = require('./routes/studentRoutes');
+const complaintRoutes = require('./routes/complaintRoutes');
 
+const app = express();
+const PORT = 3001;
 
 app.use(
     cors({
-        origin:'http://localhost:3000'
+        origin: 'http://localhost:3001'
     })
-) //we're telling the backend that it can access this url
+)
 
-mongoose.connect('mongodb://localhost:27017/new_demo')   //connecting to the database using mongoDB. URL- copy from connection in compass + 'new_demo' (database we are using).
-//if connection is not established, write 127.0.0.1 instead of localhost.
-.then(()=>{
-    console.log('connection is established')
-})
-.catch((err)=>{
-    console.log('error connecting to the Database',err.message)
+mongoose.connect('mongodb://localhost:27017/resolveradar')
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log('error connecting to the database',err.message);
+    })
+
+//middleware
+app.use(express.json());
+//app.use(userRouter);
+// Routes
+app.use('/students', studentRoutes);
+app.use('/complaints', complaintRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 })
 
-app.use(express.json()) //middleware
-app.use(userRouter)
 
-app.listen(PORT,()=>{
-    console.log(`App is served on ${PORT}`)
-})
+
+
