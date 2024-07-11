d7ac6e0f5a08673814a54e1f6e3a2cb70633ce54
MikkieGIT <114986541+MeechieMikkie693@users.noreply.github.com>
2024-07-11 18:25:11 +0530

    Updating the App.js to include the studentdash route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
+// src/App.js
 import './App.css';
 import SignUp from './SignUp/SignUp';
 import Landing from './Landing/Landing';
-import {BrowserRouter as Router, Route, Routes,Link} from 'react-router-dom'
-
+import Studentdash from './Studentdash';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Landing/>}/>
-        <Route path='/users' element={<SignUp/>}/>
+        <Route path='/' element={<Landing />} />
+        <Route path='/users' element={<SignUp onLogin={handleLogin} />} />
+        <Route path='/dashboard' element={isLoggedIn ? <Studentdash /> : <SignUp onLogin={handleLogin} />} />
       </Routes>
-  </Router>
+    </Router>
   );
 }
 
